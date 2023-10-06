8db27650ec55f8795c1e4301e80150e439a216d0
jinyayayaya <75305265+jinyayayaya@users.noreply.github.com>
2023-10-06 11:33:17 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -206,7 +206,7 @@ rebuild_firmware() {
         luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
         luci-proto-3g luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-ncm  \
         luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay  \
-        luci-app-passwall \
+        luci-app-passwall luci-app-vssr \
         luci-proto-wireguard  luci-app-wireguard kmod-wireguard wireguard-tools \
         luci-app-amlogic luci-i18n-amlogic-zh-cn \
         \
