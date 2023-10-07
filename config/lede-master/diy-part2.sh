82a1fe6bcbc4d92442f30985cad2f9138296711a
jinyayayaya <75305265+jinyayayaya@users.noreply.github.com>
2023-10-07 16:32:34 +0800

    Add files via upload

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -14,9 +14,6 @@
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
-
-
-
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
 echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
@@ -33,12 +30,6 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 #
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall.git  package/lean/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/lean/luci-app-passwall2
-
-
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
