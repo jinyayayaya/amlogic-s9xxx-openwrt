2bdfd16307190c571d89ea041c32818762680236
jinyayayaya <75305265+jinyayayaya@users.noreply.github.com>
2023-10-07 16:34:24 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -30,6 +30,9 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 #
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git  package/lean/luci-app-passwall
+
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
